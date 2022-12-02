43a0a3b37974746bcf0d7d2cfb5305891c55a9cf
jorgecursosnuevatec <jorgecursosnuevatec@gmail.com>
2022-12-02 09:09:56 +0100

    Fri Dec  2 09:09:35     2022 2022-02-07 CURSO JUNIT HASTA MOCKITO

---
+++ 01_Primer_Programa/src/com/jgr/paquetes/HolaMundo.java
@@ -0,0 +1,17 @@
+package com.jgr.paquetes;
+
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("HolaMundo");
+		int data;
+		data=230;
+		int myvar=50;
+		System.out.println(myvar);
+		
+		
+	}
+
+}
