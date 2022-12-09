9b60c46421b24f63a31a44957f975b86fccad1ec
jorgecursosnuevatec <jorgecursosnuevatec@gmail.com>
2022-12-09 18:00:23 +0100

    Fri Dec  9 18:00:23     20222022-11-08 EMPEZANDO CLOUD/KAFKA

--- basico.sh
+++ basico.sh
@@ -12,6 +12,12 @@ AddLog="/dev/null"
 CommitLog="/dev/null"
 PushLog="/dev/null"
 
+
+start=`date +%s`
+
+inicio_ns=`date +%s%N`
+inicio=`date +%s`
+
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
@@ -26,3 +32,15 @@ echo "Haciendo commit                \r\c"
 echo  "$message"
 git commit -am "$message" 1>>$CommitLog 2>>$CommitLog; git push 1>>$PushLog 2>>$PushLog
 
+end=`date +%s`
+
+runtime=$((end-start))%M
+
+fin_ns=`date +%s%N`
+fin=`date +%s`
+let total_ns=$fin_ns-$inicio_ns
+let total=$fin-$inicio
+let total_min=($total/60)
+echo "ha tardado: -$total_ns- nanosegudos, -$total- segundos, -$total_min- minutos  "
+
+echo "Finalizado,hasta luego EJERCICIOS"
