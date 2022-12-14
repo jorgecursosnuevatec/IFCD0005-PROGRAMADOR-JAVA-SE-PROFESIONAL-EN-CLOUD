af6614a4caa2d73dc5b856cd79b382ca7709011e
jorgecursosnuevatec <jorgecursosnuevatec@gmail.com>
2022-12-14 13:30:11 +0100

    Wed Dec 14 13:30:11     2022 2022-12-14 SUBIDA DESDE CLASE salida

--- basico.sh
+++ basico.sh
@@ -21,7 +21,7 @@ inicio=`date +%s`
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date) 2022-12-14 SUBIDA DESDE CLASE"
+message="$(date) 2022-12-14 SUBIDA DESDE CLASE salida"
 fi
 
 git add .
