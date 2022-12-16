4f5b41c435fdf2ae751707075ba0e582bd4c7027
jorgecursosnuevatec <jorgecursosnuevatec@gmail.com>
2022-12-16 09:03:03 +0100

    subida desde clase

--- EjerciciosClase/PRUEBAS/src/com/jgr/jorge/pruebas/Pruebas.java
+++ EjerciciosClase/PRUEBAS/src/com/jgr/jorge/pruebas/Pruebas.java
@@ -1,12 +1,14 @@
 package com.jgr.jorge.pruebas;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 
-// TODO: Auto-generated Javadoc
+
 /**
  * The Class Pruebas.
  */
@@ -18,13 +20,22 @@ public class Pruebas {
 	 * @param args the arguments
 	 */
 	public static void main(String[] args) {
-		List <Integer> lista =Stream.generate(()-> new Random()
-				.nextInt())
-				.limit(50)//10 elementos
-				.collect(Collectors.toList());
-	
-		lista.forEach(System.out::println);
-
+		
+		final int NUMERO_MAXIMO = 5;// Integer.MAX_VALUE;
+		final int NUMERO_MINIMO = 1;// Integer.MIN_VALUE;
+		
+		
+		 List <Integer> listaEnteros = new ArrayList<>();
+		 
+		 for (int i=0;i<10;i++) {
+			 listaEnteros.add(i);
+			 listaEnteros.add(i);
+		 }
+		 
+		 listaEnteros.forEach(System.out::println);
+		 listaEnteros.removeIf(p->p==2);
+		 System.out.println("\n despues");
+		 listaEnteros.forEach(System.out::println);
 	}
 
 }
