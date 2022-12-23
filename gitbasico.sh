8a5fe4ab619c5a8213189530d4473e65f494de70
jorgecursosnuevatec <jorgecursosnuevatec@gmail.com>
2022-12-23 11:26:01 +0100

    Fri Dec 23 11:26:00     20222022-12-23 ++24-Gestor-Fichero-Serializado

--- gitbasico.sh
+++ gitbasico.sh
@@ -21,7 +21,7 @@ inicio=`date +%s`
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2022-12-24 SUBIDA DESDE CLASE"
+message="$(date)2022-12-23 ++24-Gestor-Fichero-Serializado "
 fi
 
 git add .
