f2cfcfeae9273dfe3129318311b49dc07aa33040
jorgecursosnuevatec <jorgecursosnuevatec@gmail.com>
2023-01-06 11:00:13 +0100

    Fri Jan  6 10:57:10     20232023-01-06 ULTIMA SUBIDA SIN MODIFICACIONES

--- gitprune.sh
+++ gitprune.sh
@@ -14,7 +14,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2023-01-02 APUNTES DE CLASE"
+message="$(date)2023-01-06 ULTIMA SUBIDA SIN MODIFICACIONES"
 fi
 
 start=`date +%s`
