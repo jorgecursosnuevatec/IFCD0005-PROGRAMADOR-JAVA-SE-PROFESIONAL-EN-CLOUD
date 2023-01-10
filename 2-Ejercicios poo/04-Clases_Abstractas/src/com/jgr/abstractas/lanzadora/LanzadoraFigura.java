490506b9d6070f0f386c00f7fe5e062cdfea1fb1
jorgecursosnuevatec <jorgecursosnuevatec@gmail.com>
2023-01-10 13:59:06 +0100

    Tue Jan 10 13:59:05     20232023-01-10 subida desde casa

---
+++ 2-Ejercicios poo/04-Clases_Abstractas/src/com/jgr/abstractas/lanzadora/LanzadoraFigura.java
@@ -0,0 +1,23 @@
+package com.jgr.abstractas.lanzadora;
+
+import com.jgr.clases.abstractas.modelo.Circulo;
+import com.jgr.clases.abstractas.modelo.Figura;
+import com.jgr.clases.abstractas.modelo.Triangulo;
+
+/**
+ * The Class LanzadoraFigura.
+ * crear triangulo y circulo
+ */
+public class LanzadoraFigura {
+
+	public static void main(String[] args) {
+		
+		Figura cir = new Circulo("Amarillo",15);
+		Figura tri = new Triangulo("Amarillo",15,10);
+		
+		System.out.print(cir instanceof Figura);
+		
+
+	}
+
+}
